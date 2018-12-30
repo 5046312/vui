5c0da19e7d9bbfaf4e4f4ea500d9c0cd647acde4
5046312 <5046312@qq.com>
2018-12-30 20:23:18 +0800

    shit npm

--- src/components/codepanel/src/codepanel.vue
+++ src/components/codepanel/src/codepanel.vue
@@ -1,39 +1,40 @@
 <template>
-    <div class="vui-code-panel">
-        <div class="title" v-text="title" v-if="title"/>
-        <div class="panel">
-            <div class="desc" v-text="desc" v-if="title"/>
-            <div v-html="code" class="code"></div>
-            <div>
-                <div :style="{height}" class="origin-code">
-                    <v-code :code="code" ref='origin'></v-code>
-                </div>
-                <div class="show-control" @click="show = !show">{{show ? "收起源码" : "展开源码"}}</div>
-            </div>
+  <div class="vui-code-panel">
+    <div class="title" v-text="title" v-if="title"/>
+    <div class="panel">
+      <div class="desc" v-text="desc" v-if="title"/>
+      <div v-html="code" class="code"></div>
+      <div>
+        <div :style="{height: height}" class="origin-code">
+          <v-code :code="code" ref="origin"></v-code>
         </div>
+        <div class="show-control" @click="show = !show">{{show ? "收起源码" : "展开源码"}}</div>
+      </div>
     </div>
+  </div>
 </template>
 <script>
 export default {
-    name: "v-codepanel",
-    props: {
-        code: String,
-        desc: String,
-        title: String
-    },
-    data() {
-        return {
-            show: false
-        };
-    },
-    watch: {
-    },
-    methods: {},
-    computed: {
-        height () {
-            return this.show ? this.$refs.origin.$el.offsetHeight : 0
-        }
+  name: "v-codepanel",
+  props: {
+    code: String,
+    desc: String,
+    title: String
+  },
+  data() {
+    return {
+      show: false
+    };
+  },
+  
+  watch: {},
+  mounted() {},
+  methods: {},
+  computed: {
+    height() {
+      return this.show ? this.$refs.origin.$el.offsetHeight + "px" : 0;
     }
+  }
 };
 </script>
 <style lang="scss" scoped>
