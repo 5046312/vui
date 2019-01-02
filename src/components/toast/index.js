20eb7912bdd7ef180baebfe7aa58beddebac48c8
5046312 <5046312@qq.com>
2019-01-02 23:05:39 +0800

    toast 还需要考虑多次触发的问题

--- src/components/toast/index.js
+++ src/components/toast/index.js
@@ -2,28 +2,26 @@ import Vue from 'vue';
 import Toast from './src/toast'
 
 let instance;
+let timer;
 function getInstance(data) {
     if (!instance) {
         const constructor = Vue.extend(Toast);
         instance = new constructor({
             el: document.createElement('div'),
         });
+        document.body.appendChild(instance.$el);
     }
     instance.msg = data.msg
     instance.duration = data.duration
+    console.log(instance)
     return instance;
 }
 
-Vue.prototype.$toast.prototype.close = () => {
-    const el = instance.$el;
-    el.parentNode && el.parentNode.removeChild(el);
-}
-
-Vue.prototype.$toast = (msg = 'toast', duration = '2') => {
-    getInstance({msg, duration})
-    document.body.appendChild(instance.$el);
-    const timer = setTimeout(() => {
+Vue.prototype.$toast = function (msg = 'toast', duration = '2') {
+    getInstance({msg, duration}).show()
+    clearTimeout(timer)
+    timer = setTimeout(() => {
         clearTimeout(timer);
-        instance.close();
+        instance.hide()
     }, instance.duration + 100);
 };
