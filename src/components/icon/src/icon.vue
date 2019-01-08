b448a21e9abe725132193ccc4cdac36e4d733215
5046312 <5046312@qq.com>
2019-01-08 14:26:02 +0800

    font-icon 完成，准备写demo样式

--- src/components/icon/src/icon.vue
+++ src/components/icon/src/icon.vue
@@ -1,15 +1,17 @@
 <template>
-  <span class="fa fa-camera-retro fa-lg"></span>
+    <i class="vui-icon vui-ios-add"></i>
 </template>
 <script>
 export default {
-  name: "v-icon",
-  data() {
-    return {};
-  },
-  methods: {},
-  computed: {}
+    name: "v-icon",
+    data() {
+        return {};
+    },
+
+    methods: {},
+    computed: {}
 };
 </script>
 <style lang="scss" scoped>
+@import '../../../styles/components/icon/ionicons.scss';
 </style>
