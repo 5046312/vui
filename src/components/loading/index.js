54821987d20fdf816738b64d9b14c115e5af2668
5046312 <5046312@qq.com>
2019-01-24 13:28:00 +0800

    fix loading component，add new class type

--- src/components/loading/index.js
+++ src/components/loading/index.js
@@ -3,25 +3,23 @@ import Loading from './src/loading'
 import Bar from './src/bar'
 
 // loading bar
-let barInstance;
-function getInstance() {
-    if (!barInstance) {
-        const constructor = Vue.extend(Bar);
-        barInstance = new constructor({
-            el: document.createElement('div'),
-        });
-        document.body.appendChild(barInstance.$el);
+class Bar {
+    instance = null
+    static getInstance(){
+        if (!this.instance) {
+            const constructor = Vue.extend(Bar);
+            this.instance = new constructor({
+                el: document.createElement('div'),
+            });
+            document.body.appendChild(this.instance.$el);
+        }
+        return this.instance;
     }
-    return barInstance;
+    
+    start() { Bar.getInstance().start() }
+    to(percent) { Bar.getInstance().percent = percent }
+    end() { Bar.getInstance().end() }
+    fail() { Bar.getInstance().fail() }
 }
-
-const $bar = {
-    start:() =>  getInstance().start(),
-    to: percent =>  getInstance().percent = percent,
-    end:() =>  getInstance().end(),
-    fail:() =>  getInstance().fail()
-};
-
-Vue.prototype.$bar = $bar
-
-export { $bar };
+Vue.prototype.$bar = Bar.getInstance()
+export { '$bar': Bar.getInstance() };
