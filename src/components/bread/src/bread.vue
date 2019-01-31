214377aa1422ddfb0cba83b298e5fe825f48deef
5046312 <5046312@qq.com>
2019-01-31 17:12:19 +0800

    fix bread component

--- src/components/bread/src/bread.vue
+++ src/components/bread/src/bread.vue
@@ -1,27 +1,29 @@
 <template>
-    <div class='vui-bread'>
+    <div class="vui-bread">
         <div v-for="(item, index) in list" :key="index">
-            <v-icon v-if="item.icon" :type="item.icon" />
+            <v-icon v-if="item.icon" :type="item.icon" style="font-size: 16px"/>
             <span>{{item.text}}</span>
-            <span class='divide' v-if="index != list.length - 1">{{divide}}</span>
+            <span class="divide" v-if="index != list.length - 1">{{divide}}</span>
         </div>
     </div>
 </template>
 <script>
 export default {
-    name: 'v-bread',
+    name: "v-bread",
     props: {
         list: Array,
         divide: {
-            default: '/'
+            default: "/"
+        },
+        size: {
+            default: "16"
         }
     },
-    data(){
-        return {
-
-        }
-    }
-}
+    data() {
+        return {};
+    },
+    computed: {}
+};
 </script>
 <style lang="scss" scoped>
 @import "../../../styles/components/bread.scss";
