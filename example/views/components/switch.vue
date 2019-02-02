10f78664003bb6def297b44355bd63cc31cdd9e1
5046312 <5046312@qq.com>
2019-02-02 11:06:18 +0800

    fix switch component

--- example/views/components/switch.vue
+++ example/views/components/switch.vue
@@ -1,9 +1,45 @@
 <template>
     <v-layout>
         <v-demo :code="code.base">
-            <div slot="desc">进度条</div>
+            <div slot="desc">开关</div>
             <div>
                 <v-switch v-model="value" @click="value = !value"/>
+            </div>
+        </v-demo>
+
+        <v-demo :code="code.base">
+            <div slot="desc">开关 颜色</div>
+            <div>
+                <v-switch type="primary" v-model="value" @click="value = !value"/>
+                <v-switch type="info" v-model="value" @click="value = !value"/>
+                <v-switch type="danger" v-model="value" @click="value = !value"/>
+                <v-switch type="success" v-model="value" @click="value = !value"/>
+                <v-switch type="error" v-model="value" @click="value = !value"/>
+            </div>
+        </v-demo>
+
+        <v-demo :code="code.base">
+            <div slot="desc">开关 尺寸</div>
+            <div>
+                <v-switch @click="value = !value" v-model="value"/>
+                <v-switch size="small" @click="value = !value" v-model="value"/>
+            </div>
+        </v-demo>
+
+        <v-demo :code="code.base">
+            <div slot="desc">开关 内容</div>
+            <div>
+                <v-switch @click="value = !value" v-model="value" text='cn'/>
+                <v-switch @click="value = !value" v-model="value" text='en'/>
+                <v-switch @click="value = !value" v-model="value" text='icon'/>
+            </div>
+        </v-demo>
+
+        <v-demo :code="code.base">
+            <div slot="desc">开关 只读状态</div>
+            <div style="display: flex;align-items: center;">
+                <v-switch v-model="value" @click="value = !value" :readonly="readonly"></v-switch>
+                <v-button @click='readonly = !readonly'>切换只读状态</v-button>
             </div>
         </v-demo>
     </v-layout>
@@ -17,6 +53,7 @@ export default {
             code,
             percent: 0,
             value: false,
+            readonly: false,
         };
     },
     methods: {}
--- src/components/switch/src/switch.vue
+++ src/components/switch/src/switch.vue
@@ -1,14 +1,45 @@
 <template>
-    <span class='vui-switch' :class="{select: value, unselect: !value}" @click="$emit('click')">
-        <span class='text'></span>
-        <span class='icon'></span>
+    <span class='vui-switch' :class="switchClass" @click="!readonly && $emit('click')">
+        <span class='icon' :class='[type, size]'></span>
+        <span class='text' v-if="text">
+            <div class='on'>
+                <span v-if="text == 'cn'" style="padding-left: 5px;">开</span>
+                <span v-if="text == 'en'">On</span>
+                <span v-if="text == 'icon'" style="padding-left: 3px;font-size: 32px;"><v-icon type="ios-checkmark"/></span>
+            </div>
+            <div class='off'>
+                <span v-if="text == 'cn'" style="padding-left: 3px;">关</span>
+                <span v-if="text == 'en'">Off</span>
+                <span v-if="text == 'icon'" style="padding-left: 3px;font-size: 32px;"><v-icon type="ios-close"/></span>
+            </div>
+        </span>
     </span>
 </template>
 <script>
 export default {
     name: 'v-switch',
     props: {
-        value: Boolean
+        value: Boolean,
+        // 颜色
+        type: {
+            default: 'success'
+        },
+        // 尺寸 支持small
+        size: String,
+        // 只读
+        readonly: Boolean,
+        // 支持三种显示方式
+        text: String,
+    },
+    computed: {
+        switchClass(){
+            return {
+                select: this.value, 
+                unselect: !this.value, 
+                readonly: this.readonly, 
+                small: this.size == 'small'
+            }
+        },
     }
 }
 </script>
