a88498604d0b091221431efba993fd05ed1aaf2f
5046312 <5046312@qq.com>
2019-02-21 15:08:24 +0800

    打包挺坑，gulp4

---
+++ src/build/style.js
@@ -0,0 +1,25 @@
+const gulp = require('gulp');
+const cleanCSS = require('gulp-clean-css');
+const sass = require('gulp-sass');
+const rename = require('gulp-rename');
+const autoprefixer = require('gulp-autoprefixer');
+
+// css
+gulp.task('css', function () {
+    gulp.src('../styles/index.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions', 'ie > 8']
+        }))
+        .pipe(cleanCSS())
+        .pipe(rename('vui.css'))
+        .pipe(gulp.dest('../dist/styles'));
+});
+
+// 字体
+gulp.task('fonts', function () {
+    gulp.src('../src/styles/components/icon/fonts/*.*')
+        .pipe(gulp.dest('../dist/styles/fonts'));
+});
+
+gulp.task('default', gulp.series('css', 'fonts'))
