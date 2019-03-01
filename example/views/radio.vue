21b69e19980d5208386dfb2c1fe0bfccf828f3ba
5046312 <5046312@qq.com>
2019-03-01 17:02:05 +0800

    todo radio component's style bug

--- example/views/radio.vue
+++ example/views/radio.vue
@@ -4,8 +4,10 @@
             <div slot="desc">单选 Radio</div>
             <div>
                 <div style="margin-bottom: 10px;">已选择：{{value}}</div>
-                <v-radio v-model="value" val="苹果">苹果</v-radio>
-                <v-radio v-model="value" val="梨">梨</v-radio>
+                <div>
+                    <v-radio v-model="value" val="苹果">苹果</v-radio>
+                    <v-radio v-model="value" val="梨">梨</v-radio>
+                </div>
             </div>
         </div>
     </div>
