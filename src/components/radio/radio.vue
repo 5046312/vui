78cdb3235cce72957dfa2a367a9f2a887b4c8c77
5046312 <5046312@qq.com>
2019-03-04 16:00:14 +0800

    fix radio transform effect

--- src/components/radio/radio.vue
+++ src/components/radio/radio.vue
@@ -1,7 +1,9 @@
 <template>
     <span class='vui-radio' @click="$emit('input', val)">
         <span class='radio' :class="{active: value == val}">
-            <v-icon type="ios-checkmark" v-if="value == val"/>
+            <div class='icon'>
+                <div class="round" :class="{active: value == val}"></div>
+            </div>
         </span>
         <span class='label'><slot /></span>
     </span>
